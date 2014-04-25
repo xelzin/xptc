159bf583fa432e7fbfcf5172072421d0902ef070
HD_NVR <HD_NVR@Devel_WIN>
2014-04-25 10:05:20 +0200

    Start files

---
+++ XPTC/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Git base file
+-->
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title></title>
+   </head>
+   <body>
+      <?php
+      // put your code here
+      ?>
+   </body>
+</html>
